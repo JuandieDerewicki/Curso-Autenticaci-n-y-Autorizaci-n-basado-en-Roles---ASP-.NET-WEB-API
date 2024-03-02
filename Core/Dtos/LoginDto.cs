14c36a2eaa0c9242d02083975d34e037f97cf9e0
JuandieDerewicki <jdderewicki@gmail.com>
2024-03-02 15:06:50 -0300

    Agregué la carpeta DTOs con sus respectivas clases de login, update y register. Tambien agregué la carpeta OtherObjects donde se encuentra StaticRoles con los roles predeterminados

---
+++ Core/Dtos/LoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAutorizacionAutenciacionEnRoles.Core.Dtos
+{
+    public class LoginDto
+    {
+        // Los DTOs son objetos que se utilizan para transportar datos entre diferentes componentes de un sistema, como capas de una aplicacion o servicios web. Generalmente se usan en aplicaciones donde hay una separacion entre las capas de presentacion, logica de negocio y acceso a datos
+        // En este caso se usan para transportar datos entre los controladores de la API (donde se reciben las solicitudes HTTP) y los servicios de aplicacion (donde se procesan y se realizan las operacioens de negocio) Los DTOs pueden contener los datos necesarios para las operaciones de autenticacion y autorizacion
+
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,14 +12,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Agregar bd
+// Agregar contexto de la bd que se utiliza para interactuar con la bd y gestionar usuarios y roles a traves de identity
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("local");
     options.UseSqlServer(connectionString);
 });
 
-// Agregar Identity
+// Agregar Identity que es un marco de trabajo que permite la gestion de usuarios, roles, autenticacion y autorizacion 
 builder.Services
     .AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
@@ -36,9 +36,9 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.SignIn.RequireConfirmedEmail = false;
 });
 
-// Agregar autenticación y Jwt
+// Agregar autenticación y Jwt donde estamos usando JWT para la autenticacion basada en tokens ya que JWT es un estandar abierto (RFC 7519) que define de forma compacta y autonoma de transmitir info de forma segura entre dos partes como un objeto JSON
 builder.Services
-    .AddAuthentication(options =>
+    .AddAuthentication(options => // Se configura la autenticacion JWT para que utilice un esquema de autenticacion basado en tokens
     {
         options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -50,7 +50,7 @@ builder.Services
         options.RequireHttpsMetadata = false;
         options.TokenValidationParameters = new TokenValidationParameters()
         {
-            ValidateIssuer = true,
+            ValidateIssuer = true, // Especifica los parametros de validacion del token JWT, como la validacion del emisor, la audiencia y la clave de la firma, estos parametros se usan para validar y decodificar los tokens JWT enviados a los clientes
             ValidateAudience = true,
             ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
             ValidAudience = builder.Configuration["JWT:ValidAudience"],
