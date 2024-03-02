5bb673d1c34e507c0d872c58eaf58915a93f824e
JuandieDerewicki <jdderewicki@gmail.com>
2024-03-02 10:21:07 -0300

    Refactorizando WeatherForecastController

--- obj/Debug/net8.0/JwtAutorizacionAutenciacionEnRoles.AssemblyInfo.cs
+++ obj/Debug/net8.0/JwtAutorizacionAutenciacionEnRoles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtAutorizacionAutenciacionEnRoles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cee52f6e190415075c4fa7d0c39fe8edc472db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("JwtAutorizacionAutenciacionEnRoles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JwtAutorizacionAutenciacionEnRoles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
